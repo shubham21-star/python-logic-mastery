82f0ff36eabc0c15e01f2422280751785e6c15a9
shubham21-star <shubham.asp01@gmail.com>
2026-02-13 16:16:41 +0530

    .\03_user_math_calculation.py

---
+++ 03_college_notes/01_Basics/03_user_math_calculation.py
@@ -0,0 +1,10 @@
+# write a program to solve given mathematical equation
+
+n= int(input("Enter number (n):  "))
+a= int(input("Enter number (a):  "))
+b= int(input("Enter number (b):  "))
+c= int(input("Enter number (c):  "))
+
+print(n*(a*b*c)**(1/2))
+
+
