468ec0d589724b03828b2e3ee3b75b7321b32fa6
shubham21-star <shubham.asp01@gmail.com>
2026-02-14 10:54:46 +0530

    correction in folders

--- 01_Basics/02_statistics_program.py
+++
@@ -1,8 +0,0 @@
-import statistics
-data = [10,20,30,40,50,60]
-m1= statistics.mean(data)
-print("mean:",m1)
-m2= statistics.median(data)
-print("median:",m2)
-m3= statistics.mode(data)
-print("mode:",m3)
