5c0f163f538db23dee11da233e013f78fadeaa7f
shubham21-star <shubham.asp01@gmail.com>
2026-02-23 01:22:45 +0530

    .\04_length_of_strings.py

---
+++ 02_Strings_&_Conditional_Statements/04_length_of_strings.py
@@ -0,0 +1,14 @@
+str = "Apna College"
+print(len(str))
+
+str1 = "Sunstone"
+print(len(str1))
+
+str2 = "Shubham"
+print(len(str2))
+
+str3 = "Harsh"
+print(len(str3))
+
+str4 = "Data Science"
+print(len(str4))
