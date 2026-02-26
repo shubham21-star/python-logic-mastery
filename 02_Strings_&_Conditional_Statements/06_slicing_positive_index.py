0a48350301af0335a4c90e98a1768dfbaf8147c1
shubham21-star <shubham.asp01@gmail.com>
2026-02-27 02:12:35 +0530

    .\06_slicing_positive_index.py

---
+++ 02_Strings_&_Conditional_Statements/06_slicing_positive_index.py
@@ -0,0 +1,17 @@
+str = "ApnaCollege"
+
+print(str[1:4])   # pna
+print(str[:4])    # Apna
+print(str[1:])    # pnaCollege
+
+str1 = "Shubham Kumar"
+
+print(str1[0:3])   
+print(str1[6:10])   
+print(str1[7:12])  
+
+str2 = "Sunstone"
+
+print(str2[1:5])  
+print(str2[3:6])   
+print(str2[2:4])  
