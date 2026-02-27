1a8f84bd6b8c20a2e8c70da39afe5c4332d2c6a5
shubham21-star <shubham.asp01@gmail.com>
2026-02-28 03:00:00 +0530

    .\07_slicing_negative_index.py

---
+++ 02_Strings_&_Conditional_Statements/07_slicing_negative_index.py
@@ -0,0 +1,14 @@
+str = "Apple"
+print(str[-3:-1]) # pl
+
+str1 = "Shubham Kumar"
+
+print(str1[-1:-4])   
+print(str1[-3:-5])   
+print(str1[-10:12])  
+
+str2 = "Sunstone"
+
+print(str2[-1:-3])   
+print(str2[-2:-5])   
+print(str2[-4:-6])  
