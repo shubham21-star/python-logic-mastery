d500937772dcf67774a443f5dd4d20bc3f7030e2
shubham21-star <shubham.asp01@gmail.com>
2026-03-09 10:42:21 +0530

    .\13_string_function.py

---
+++ 02_Strings_&_Conditional_Statements/13_string_function.py
@@ -0,0 +1,19 @@
+str = "I am a coder."
+
+print(str.endswith("er."))     # True
+print(str.count("am"))         # 1
+print(str.capitalize())        # I am a coder.
+print(str.find("coder"))       # index of 'c'
+print(str.replace("coder", "developer"))
+
+# common function
+str = "i am learning python"
+
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.title())
+print(str.count("a"))
+print(str.find("python"))
+print(str.replace("python", "coding"))
+
