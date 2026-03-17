aa245e37d9e6ee749776e93c7b45abecdd350c63
shubham21-star <shubham.asp01@gmail.com>
2026-03-17 05:39:15 +0530

    .\16_strip_&_split.py

---
+++ 02_Strings_&_Conditional_Statements/16_strip_&_split.py
@@ -0,0 +1,13 @@
+# Remove Spaces
+str = "   hello   "
+print(str.strip())
+
+# Splitting Strings
+str = "I am learning Python"
+print(str.split())
+
+# WAP 
+sentence = input("Enter sentence: ")
+words = sentence.split()
+
+print("Words count =", len(words))
