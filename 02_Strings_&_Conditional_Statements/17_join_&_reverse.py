2d581cb31663dd4fd4a684f8e7a81177219b344d
shubham21-star <shubham.asp01@gmail.com>
2026-03-18 01:00:00 +0530

    .\17_join_&_reverse.py

---
+++ 02_Strings_&_Conditional_Statements/17_join_&_reverse.py
@@ -0,0 +1,11 @@
+# Joining Strings
+words = ["Apna", "College", "Python"]
+print(" ".join(words))
+
+# Reverse a String
+str = "Python"
+print(str[::-1])
+
+# WAP
+str = input("Enter string: ")
+print("Reverse =", str[::-1])
