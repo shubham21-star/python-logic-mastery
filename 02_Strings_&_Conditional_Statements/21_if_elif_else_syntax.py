795f56bd4369d708624af33c5196b0343611662f
shubham21-star <shubham.asp01@gmail.com>
2026-03-21 12:27:22 +0530

    add 21_if_elif_else_syntax.py

---
+++ 02_Strings_&_Conditional_Statements/21_if_elif_else_syntax.py
@@ -0,0 +1,21 @@
+# if condition:
+#     statement1
+# else:
+#     statement2
+
+num = 5
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+age = int(input("Enter age: "))
+
+if age >= 18:
+    print("Adult")
+elif age >= 13:
+    print("Teenager")
+else:
+    print("Child")
