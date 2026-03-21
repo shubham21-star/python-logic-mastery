dc9d101d20bb7af79c63b5015e78800bb4e613bf
shubham21-star <shubham.asp01@gmail.com>
2026-03-21 12:23:43 +0530

    add 20_alphabetical_larger_string.py

---
+++ 02_Strings_&_Conditional_Statements/20_alphabetical_larger_string.py
@@ -0,0 +1,8 @@
+# WAP: Alphabetically Larger String
+a = input("Enter first string: ")
+b = input("Enter second string: ")
+
+if a > b:
+    print(a, "comes after", b)
+else:
+    print(b, "comes after", a)
