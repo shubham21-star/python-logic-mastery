82e6cee067bf0fec75132b21e5a42fe082aceb89
shubham21-star <shubham.asp01@gmail.com>
2026-07-13 11:44:22 +0530

    .\03_Odd_Even_Check.py

---
+++ 03-Holi-Work/03_Odd_Even_Check.py
@@ -0,0 +1,12 @@
+# 03_Odd_Even_Check.py
+
+T = int(input("Enter a number: "))
+
+print("Name: Shubham Kumar")
+
+if T % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+print("Name: Shubham Kumar")
