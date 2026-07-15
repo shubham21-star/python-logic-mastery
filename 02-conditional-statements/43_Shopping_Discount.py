47c0721047098f0d23a78436192183458b978f22
shubham21-star <shubham.asp01@gmail.com>
2026-07-15 10:22:44 +0530

    .\43_Shopping_Discount.py

---
+++ 02-conditional-statements/43_Shopping_Discount.py
@@ -0,0 +1,10 @@
+#Shopping mall
+spend = int(input("Enter your Spending Amount $:")) 
+age = int(input("Enter your age:"))
+ 
+if(spend >= 100 or age >= 60):
+
+    print("Your are Eligiable for 20% Discount") 
+else:
+    print("Doesn't Eligiable for 20% Discount")
+    
