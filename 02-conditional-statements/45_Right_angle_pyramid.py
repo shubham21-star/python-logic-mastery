ce6095324e8cb8afd157a9144c308f2df2fe7748
shubham21-star <shubham.asp01@gmail.com>
2026-07-16 23:03:47 +0530

    .\45_Right_angle_pyramid.py

---
+++ 02-conditional-statements/45_Right_angle_pyramid.py
@@ -0,0 +1,38 @@
+#code1
+for i in range(1,5):
+    for j in range(1,i+1):
+        print("* ", end=" ")
+    print()
+print()
+
+#code2
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(i , end=" ")
+    print()
+print()
+
+#code3
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(j , end=" ")
+    print()
+print()
+
+#code4
+num=0
+for i in range(1,4):
+    for j in range(1,i+1):
+        num+=1
+        print(num , end=" ")
+    print()
+print()
+
+#code5
+mul = 0
+for i in range(1,4):
+    for j in range(1,i+1):
+        mul+=2
+        print(mul , end=" ")
+    print()
+print()
